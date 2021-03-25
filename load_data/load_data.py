6408d9f3dab16b01e28797b4e8500164dc433c6a
opeyem1a <opeyadeyemi@gmail.com>
2021-03-25 04:06:54 -0700

    data is loaded from the csv

--- load_data/load_data.py
+++ load_data/load_data.py
@@ -1,5 +1,7 @@
 import csv
 import pandas as pd
+import math
+import random
 
 from models import Graph
 
@@ -13,23 +15,21 @@ class LoadData:
                     'Flight_Number_Marketing_Airline', 'Flight_Number_Operating_Airline', 'Origin', 'OriginAirportID',
                     'Dest', 'DestAirportID', 'DepTime', 'ArrTime', 'CRSElapsedTime', 'ActualElapsedTime']
         df = pd.read_csv('./data/flight_data.csv', usecols=col_list)
-        print(df.loc[0, :])
-        print(df["Marketing_Airline_Network"].unique())
-        print(df["Origin"].unique())
-        print(df["Dest"].unique())
-        # TODO: make all airport objects
 
-        # TODO: make all flights to connect airports
-        #  (just calling add flights should create the required airport objects too)
-        # for index, row in df.iterrows():
-        #     print(row['Origin'], row['Dest'])
+        agg_data = df.groupby(['Marketing_Airline_Network',
+                               'Flight_Number_Operating_Airline',
+                               'Origin',
+                               'Dest',
+                               'DayOfWeek']) \
+            .agg({'DepTime': ['mean'],
+                  'ActualElapsedTime': ['mean'], }) \
+            .dropna()
 
-        print(df.groupby(['Origin', 'Dest']).agg())
-        # with open('./data/flight_data.csv') as file:
-        #     csv_reader = csv.reader(file, delimiter=',')
-        #     line_count = 0
-        #     for row in csv_reader:
-        #         if line_count == 0:
-        #             print(f'Column names are {", ".join(row)}')
-        #         line_count += 1
-        #     print(f'Processed {line_count} lines.')
+        for index, row in agg_data.iterrows():
+            src_code = row.name[2]
+            dest_code = row.name[3]
+            dept_time = row['DepTime']
+            duration = row['ActualElapsedTime']
+            # TODO: obviously the price should not be random going forward
+            price = random.randint(100, 1000)
+            self.graph.create_flight(src_code, dest_code, dept_time, duration, price)

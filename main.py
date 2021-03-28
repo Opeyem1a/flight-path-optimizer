0cde9901b84ffefc21b3e5a21e5270a1386a7c4e
opeyem1a <opeyadeyemi@gmail.com>
2021-03-28 02:12:44 -0700

    + updated entrypoint + catch some errors + print some diagnostic info about the airport graph

--- main.py
+++ main.py
@@ -1,14 +1,51 @@
+import time
+
+import dill as dill
+
 from load_data import LoadData
 from models import Graph
 
+prelim = input("\"load\" or \"open\"\n")
 graph = Graph()
-ld = LoadData(graph)
-ld.load()
+max_outgoing = int(input("Enter max # of outgoing flights per airport: "))
+
+if prelim == 'load':
+    ld = LoadData(graph, True, max_outgoing)
+    ld.load()
+else:
+    ld = LoadData(graph, False, max_outgoing)
+    ld.load()
+
 print(graph)
+print('Number of Airports: ', len(graph.airport_dict))
+num_edges = 0
+for a in graph.airports:
+    num_edges += len(a.outgoing_flights)
+print('Number of Flights (Edges): ', num_edges)
 
 while True:
-    user_input = input("Hit enter to to run the function again.")
+    try:
+        src_code = input("First Airport Code: ")
 
-    from algorithm import FlightOptimizer
-    fo = FlightOptimizer('a', 'b')
+        src_airport = graph.get_airport(src_code.upper())
+        src_airport.print_flights()
 
+        dest_code = input("Second Airport Code: ")
+
+        dest_airport = graph.get_airport(dest_code.upper())
+        dest_airport.print_flights()
+
+        start_time = int(input('Start Time: '))
+
+        if src_airport is None or dest_airport is None:
+            print('Something went wrong. Try again.\n')
+            continue
+
+        from algorithm import FlightOptimizer
+
+        fo = FlightOptimizer(src_airport, dest_airport, start_time)
+        fo.find_best_path()
+    except KeyError:
+        continue
+    except ValueError:
+        continue

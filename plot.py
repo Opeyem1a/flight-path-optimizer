3293a6daa587981d23cb84e08bbddcb23ad21559
opeyem1a <opeyadeyemi@gmail.com>
2021-04-09 11:25:01 -0700

    data for plots and updated plot file coefficients

--- plot.py
+++ plot.py
@@ -26,13 +26,13 @@ def plot_line(best_alg: bool, vary: string):
     if is_v and not best_alg:
         b = const_num_flights
         b2 = b ** 2  # i.e. b^2
-        y = [0.00000000007 * (V ** 2) * b2 for V in x]
+        y = [0.00000000016 * (V ** 2) * b2 for V in x]
         plt.plot(x, y, label='Predicted - O(b^2 * V^2)')
 
     if is_v and best_alg:
         b = const_num_flights
         for V in rng:
-            y.append(0.000000002 * b * (V ** 2))
+            y.append(0.00000001 * b * (V ** 2))
             y2.append(0.00000002 * b * V * math.log(V))
         plt.plot(x, y, label='Predicted - O(bV^2)')
         plt.plot(x, y2, label='Predicted - O(bVlog(V))')
@@ -40,8 +40,8 @@ def plot_line(best_alg: bool, vary: string):
     if not is_v and best_alg:
         V = const_num_airports
         for b in rng:
-            y.append(0.000000005 * b * (V**2))
-            y2.append(0.00000012 * b * V * math.log(V))
+            y.append(0.000000007 * b * (V**2))
+            y2.append(0.00000015 * b * V * math.log(V))
         plt.plot(x, y, label='Predicted - O(bV^2)')
         plt.plot(x, y2, label='Predicted - O(bVlog(V))')
 
@@ -62,7 +62,7 @@ def plot_runtime(best_alg: bool, vary: string):
     y = []
 
     for time in times:
-        y.append(0 - time)  # TODO: will be positive in the real run, so no need to flip the signs
+        y.append(time)
 
     x_label = 'V (number of airports)' if is_v else 'b (max number of outgoing flights)'
     line_label = 'Better Algorithm' if best_alg else 'Worse Algorithm'
